1d9a0797bdd226e025ae862321db193cebe2441e
Payton Viloria <paytonviloria@gmail.com>
2021-03-08 14:37:11 -0800

    Finished lab mock data

--- mock-data.js
+++ mock-data.js
@@ -43,7 +43,97 @@ let mock = [{
     submitted: false,
     grade: 0
   },
-
+  //Labs section
+  {
+    id: 6,
+    name: "Lab 1",
+    description: "This is the first lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "April 1",
+    submitted: true,
+    grade: 35
+  },
+  {
+    id: 7,
+    name: "Lab 2",
+    description: "This is the second lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "April 7",
+    submitted: true,
+    grade: 50
+  },
+  {
+    id: 8,
+    name: "Lab 3",
+    description: "This is the third lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "April 23",
+    submitted: true,
+    grade: 45
+  },
+  {
+    id: 9,
+    name: "Lab 4",
+    description: "This is the fourth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 1",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 10,
+    name: "Lab 5",
+    description: "This is the fifth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 6",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 11,
+    name: "Lab 6",
+    description: "This is the sixth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 10",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 12,
+    name: "Lab 7",
+    description: "This is the seventh lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 15",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 13,
+    name: "Lab 8",
+    description: "This is the eighth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 20",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 14,
+    name: "Lab 9",
+    description: "This is the ninth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 30",
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 15,
+    name: "Lab 10",
+    description: "This is the tenth lab of the course. Labs test what you learned in class. Review Class Material.",
+    points: 50,
+    dueDate: "May 31",
+    submitted: false,
+    grade: 0
+  },
 ]
 
 export default mock;
