3d43cf5d1e4210f64c34072e807d30db9a0b8740
Payton Viloria <paytonviloria@gmail.com>
2021-03-08 14:28:26 -0800

    Finished exam mock data

---
+++ mock-data.js
@@ -0,0 +1,49 @@
+let mock = [{
+    id: 1,
+    name: "Exam One",
+    description: "First midterm of the course. This is a summative assessment. Make sure to study.",
+    points: 100,
+    dueDate: "April 1",
+    submitted: true ,
+    grade: 90
+  },
+  {
+    id: 2,
+    name: "Exam Two",
+    description: "Second midterm of the course. This is a summative assessment. Make sure to study.",
+    points: 100,
+    dueDate: 'April 5',
+    submitted: true,
+    grade: 95
+  },
+  {
+    id: 3,
+    name: "Exam Three",
+    description: "Third exam of the course. This is a summative assessment. Make sure to study.",
+    points: 100,
+    dueDate: 'April 20',
+    submitted: true,
+    grade: 70
+  },
+  {
+    id: 4,
+    name: "Exam Four",
+    description: "Fourth exam of the course. This is a summative assessment. Make sure to study.",
+    points: 100,
+    dueDate: 'May 1',
+    submitted: false,
+    grade: 0
+  },
+  {
+    id: 5,
+    name: "Exam Five",
+    description: "Fifth exam of the course. This is a summative assessment. Make sure to study.",
+    points: 100,
+    dueDate: 'May 10',
+    submitted: false,
+    grade: 0
+  },
+
+]
+
+export default mock;
