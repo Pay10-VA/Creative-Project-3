3de14c97c68fbd6c66d7470cce9d27ee6079cc8b
Payton Viloria <paytonviloria@gmail.com>
2021-03-11 20:46:08 -0800

    Chat input and button styles complete

--- learning-management/src/main.js
+++ learning-management/src/main.js
@@ -9,6 +9,24 @@ let data = {
   assignments: mock,
   cart: [],
   view: 1,
+  posts: [
+    {
+      name: "Shaun Jerry",
+      message: "Could anyone help me with problem 10 on homework #12? I can't figure it out."
+    },
+    {
+      name: "Charlie Goodman",
+      message: "For problem 10, make sure to carry the one. Everything is self explanatory from there."
+    },
+    {
+      name: "Shaun Jerry",
+      message: "Thank you! I can't believe I overlooked that step."
+    },
+    {
+      name: "Professor Ren (Instructor)",
+      message: "Just a reminder that today's lab has been pushed back two weeks to give you all more time to work on today's homework. Please come in to office hours should you need any help."
+    },
+  ]
 }
 
 
--- learning-management/src/views/Forum.vue
+++ learning-management/src/views/Forum.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="forum-main"></div>
+  <div id="forum-main">
+
+    <h1>Class Chat</h1>
+
+    <p>Get help from classmates, TA's, and the course instructor. Post your questions or converse with your fellow classmates</p>
+
+    <div id="post-header">
+      <h3>150 Participants</h3>
+    </div>
+    <div id="posts">
+      <div v-for="post in this.$root.$data.posts" :key="post.name" id="single-post">
+        <div id="post-name">
+          <h3>{{post.name}}</h3>
+        </div>
+        <div id="post-message">
+          <p>{{post.message}}</p>
+        </div>
+      </div>
+    </div>
+
+    <div id="user-posting">
+      <input>
+      <button><i class="far fa-paper-plane"></i></button>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -14,4 +39,64 @@ export default {
   #forum-main {
     min-height: calc(100vh - 120px - 57px);
   }
+
+  #posts {
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  #single-post {
+    border: solid;
+    border-width: thin;
+    margin-bottom: 5px;
+  }
+
+  #post-name {
+    text-align: left;
+    padding-left: 5px;
+    padding-bottom: 5px;
+    color: #325A7F;
+  }
+
+  #post-message {
+    text-align: left;
+    padding-left: 5px;
+    padding-bottom: 5px;
+  }
+
+  #post-header {
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-bottom: 5px;
+    border: solid;
+    border-width: thin;
+    background-color: #f2f2f2;
+    margin-top: 20px;
+    text-align: left;
+  }
+
+  #user-posting {
+    display: flex;
+    width: 85%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  input {
+    width: 85%;
+    height: 30px;
+  }
+
+  button {
+    width: 15%;
+    height: 35px;
+  }
+
+  button:hover {
+    background-color: #f2f2f2;
+  }
+
+
 </style>
